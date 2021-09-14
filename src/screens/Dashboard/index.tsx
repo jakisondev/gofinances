f19993f1ad0c37477799ccbe5ca6979789c05330
jakisondev <jakison.dev@gmail.com>
2021-09-14 10:36:18 -0300

    CardTransactions

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
+
 import {
     Container,
     Header,
@@ -8,10 +13,57 @@ import {
     User,
     UserGreeting,
     UserName,
-    Icon
+    Icon,
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 } from './styles';
 
+
 export function Dashboard() {
+    const data = [
+        {
+            title: "Desenvolvimento de Site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+
+            date: "13/04/2020"
+        },
+        {
+            title: "Desenvolvimento de Site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+
+            date: "13/04/2020"
+        },
+        {
+            title: "Desenvolvimento de Site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+
+            date: "13/04/2020"
+        },
+        {
+            title: "Desenvolvimento de Site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+
+            date: "13/04/2020"
+        },
+    ];
     return (
         <Container>
             <Header>
@@ -29,6 +81,41 @@ export function Dashboard() {
                     <Icon name="power" />
                 </UserWrapper>
             </Header>
+
+            <HighlightCards>
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,000"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,000"
+                    lastTransaction="Última saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,000"
+                    lastTransaction="01 à 16 de abril"
+                />
+            </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionList
+                    data={data}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingBottom: getBottomSpace(),
+                    }}
+                />
+
+            </Transactions>
+
         </Container>
     )
 }
