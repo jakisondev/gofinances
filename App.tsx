44fd56218db8fc7ee933a606ffe82ac8934149a0
unknown <jakison.dev@gmail.com>
2021-10-05 22:03:48 -0300

    SignInScreen

--- App.tsx
+++ App.tsx
@@ -20,6 +20,8 @@ import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 
+import { SignIn } from './src/screens/SignIn';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -37,7 +39,8 @@ export default function App() {
         <StatusBar
           barStyle="light-content"
         />
-        <AppRoutes />
+        <SignIn/>
+        {/* <AppRoutes /> */}
       </NavigationContainer>
     </ThemeProvider>
   )
